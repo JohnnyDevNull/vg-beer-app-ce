28229753e82671af1258d321270b4a909337ef1c
Philipp John <philipp.john@live.de>
2024-02-25 00:14:00 +0100

    feat: add beer page content

--- src/views/Beer/index.tsx
+++ src/views/Beer/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Beer as IBeer } from '../../types';
-import { fetchData } from './utils';
+import { extractBeerParams, fetchData } from './utils';
 import { useParams } from 'react-router-dom';
 
 const Beer = () => {
@@ -10,6 +10,11 @@ const Beer = () => {
   // eslint-disable-next-line
   useEffect(fetchData.bind(this, setBeer, id), [id]);
 
+  const beerParams = beer ? extractBeerParams(beer) : [];
+  const addressString = beerParams.join(', ');
+  const beerUriString = encodeURI(addressString);
+  const mapsUrl = `https://www.google.com/maps/embed/v1/search?key=YOUR_API_KEY&q=${beerUriString}`;
+
   return (
     <article>
       <section>
@@ -18,7 +23,19 @@ const Beer = () => {
         </header>
         <main>
           <span>
-            <b>Type: </b> {beer?.brewery_type}
+            <p><b>Type:</b> {beer?.brewery_type}</p>
+            <p><b>Address:</b> {addressString} </p>
+            {beer?.phone && <p><b>Phone:</b> {beer?.phone} </p>}
+            {beer?.website_url && <p><b>Website:</b> <a href={beer?.website_url} target="_blank" rel="nofollow noopener">{beer?.website_url}</a> </p>}
+
+            <iframe
+              width="100%"
+              height="500"
+              loading="lazy"
+              allowFullScreen
+              referrerPolicy="no-referrer-when-downgrade"
+              src={mapsUrl}>
+            </iframe>
           </span>
         </main>
       </section>
--- src/views/Beer/utils.ts
+++ src/views/Beer/utils.ts
@@ -1,5 +1,5 @@
 import { getBeer } from '../../api';
-import { Beer } from '../../types';
+import { Beer as IBeer, Beer } from '../../types';
 import handle from '../../utils/error';
 
 const fetchData = (setData: (data: Beer) => void, id?: string) => {
@@ -15,4 +15,34 @@ const fetchData = (setData: (data: Beer) => void, id?: string) => {
   })();
 };
 
-export { fetchData };
+function extractBeerParams(beer: IBeer): string[] {
+  const params: string[] = [];
+
+  if (beer?.name) {
+    params.push(beer.name);
+  }
+
+  if (beer?.street) {
+    params.push(beer.street);
+  }
+
+  if (beer?.city) {
+    params.push(beer.city);
+  }
+
+  if (beer?.postal_code) {
+    params.push(beer.postal_code);
+  }
+
+  if (beer?.state) {
+    params.push(beer.state);
+  }
+
+  if (beer?.country) {
+    params.push(beer.country);
+  }
+
+  return params;
+}
+
+export { fetchData, extractBeerParams };
